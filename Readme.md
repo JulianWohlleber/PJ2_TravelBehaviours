c3b617bb99381e1f13dc1a9a9b60efda48e0e6be
suparsumo <julianlucaswohlleber@me.com>
2015-04-01 00:11:48 +0200

    Upload

---
+++ Readme.md
@@ -0,0 +1,21 @@
+#PRJ2_TravelBehaviours
+This was probably one of the hardest task, I had in this course. The task was to generate Data and do a Datavisualisation with it, which should later be published on a Webpage online.
+
+##IDEA  
+After some time thinking, I decided to generate a Survey about the daily travel distances and their impact on the transportation-method and the effects of health. I generated the Survey with Google Survey, because google Survey offers the best ways to publish the generated data, which later was really relevant for the built of the Html-Page.   
+Get to the [Survey](https://docs.google.com/forms/d/1c1C4VeYMhYuB85N2pcHSz9LSODnzceXqUcMo2GKArtI/viewform?c=0&w=1).
+
+##REALISATION - Website 1.0
+When I started to write the Webpage, I first was really really lost. I never had made any experience before with html, much less I had never heard of css. The only thing that I understood a little was java script with the help of p5, which is a library that makes java script feel and work pretty similar to processing.
+When I succeeded after a few hours to built a Webpage like I wanted it (Html only, no Java Script, precasted css), I decided to start with the java script part. After hours of work, I had... yeah - actually still nothing. I simply didn't understand arrays, and how I could use the contend of the google spreadsheet in my code. After some more time struggeling I felt really low, and decided to just built the Visualisation manually with Illustrater, export the document as a PDF and and just implement that into my webpage.  I wasn't really proud of this solution, but I just didn't had the time to finish it properly.  
+
+Get to my [webpage 1.0](http://www.julianlucas.de/_/).
+
+##Finish
+When my vacation started and I had done all my dokus for my other subject, I decided to finish, what I started so far. After an 8-hour-youtube-course I understood the basics of java script. I could now use arrays, link a java script into html, link buttons of a html page to java script (after some time of practise), and after some more hours of research was able to use the data of the online spreadsheet via tabletop.  
+I searched for a nice css, built the html page, and after a lot of time, also finished the java script.  
+I'm really proud that I've built my first Webpage, and I'm happy, that I invested so much time, because Coding seems to be like reading or writing. Once you've learned something, you do not forget it, or at least learn it fast again.  
+There are still some issues I had with the page, for example that I wasn't able to fit the Canvas into the window-width, which sometimes generates unlovely grades between the html section and the canvas, but I think I should be able to fix this in some time.
+
+
+To my [final page](http://julianlucas.de/TravelBehaviours/).
---
+++ TravelBehaviours/main.js
@@ -0,0 +1,143 @@
+var bike, plane, local, walk, train, auto, boat;
+var importedData = null;
+var radius = new Array();
+var r = new Array();
+var g = new Array();
+var b = new Array();
+var healthstatus = new Array();
+var laenge;
+var k = 11;
+var S = 1300;
+var tMode = new Array();
+var resetter = 0;
+var Color = 56;
+
+document.addEventListener('DOMContentLoaded', function() {
+    var URL = "https://docs.google.com/spreadsheets/d/1OPrbGMRBIWU88dPVyN-6wqTkqG_QA-nj0jpT4sOGBKM/pubhtml"
+    Tabletop.init( { key: URL, callback: myData, simpleSheet: true } )
+})
+
+function setup() {
+var myCanvas = createCanvas(S,420);
+    myCanvas.parent('jsCanvas');
+setFrameRate(120);
+}
+
+function myData(data){
+  console.log(data);
+
+  laenge = data.length;
+
+  for(var i = 0; i<data.length; i++){
+    //circle colors
+    if(data[i].whatisyourprimarymodeoftransportationtoyourplaceofworkstudytraining == "bicycle"
+ == "bicycle"){
+      tMode[i] = bike;
+      r[i] = 250;
+      g[i] = 5;
+      b[i] = 19;
+
+    }
+    if(data[i].whatisyourprimarymodeoftransportationtoyourplaceofworkstudytraining == "walking"){
+      tMode[i] = walk;
+      r[i] = 255;
+      g[i] = 165;
+      b[i] = 0;
+
+    }
+    if(data[i].whatisyourprimarymodeoftransportationtoyourplaceofworkstudytraining == "local public transport"){
+      tMode[i] = local;
+      r[i] = 0;
+      g[i] = 120;
+      b[i] = 250;
+    }
+    if(data[i].whatisyourprimarymodeoftransportationtoyourplaceofworkstudytraining == "train"){
+      tMode[i] = train;
+      r[i] = 0;
+      g[i] = 160;
+      b[i] = 250;
+    }
+    if(data[i].whatisyourprimarymodeoftransportationtoyourplaceofworkstudytraining == "personal automobile"){
+      tMode[i] = auto;
+      r[i] = 0;
+      g[i] = 200;
+      b[i] = 150;
+    }
+    if(data[i].whatisyourprimarymodeoftransportationtoyourplaceofworkstudytraining == "boat"){
+      tMode[i] = boat;
+      r[i] = 0;
+      g[i] = 200;
+      b[i] = 200;
+    }
+    if(data[i].whatisyourprimarymodeoftransportationtoyourplaceofworkstudytraining == "plane"){
+      tMode[i] = plane;
+      r[i] = 200;
+      g[i] = 200;
+      b[i] = 30;
+    }
+    radius[i] = data[i].whatisthetraveldistancebetweenyourhomeandyourplaceofworkstudytraining;
+
+    if(data[i].haveyoubeenillthelast3weeks == "Yes"){
+      healthstatus[i] = 1;
+    }
+    if(data[i].haveyoubeenillthelast3weeks == "No"){healthstatus[i] = 0
+    }
+  }
+
+}
+
+function sall() {
+  if(resetter != 1){background(Color);    resetter = 1;}
+  for(var i = 0; i<laenge; i++){
+    noFill();
+    strokeWeight(1);
+    stroke(r[i],g[i],b[i]);
+
+    var x = radius[i];
+    // "S-c* a^x" beschränktes Wachstum, um unendlich große Kreise zu verhindern -- umso höher die Werte für radius, je geringer der unterschied zur nächstgrößeren größe
+    //var rFormel = S-k* a^(x*10); -- habe mich letztendlich dagegen entschieden, da dadurch die weit entfernten distanzen extrem miteinander verschwimmen
+    var rFormel = k * x;
+    ellipse(150,300, rFormel, rFormel);
+  }
+}
+
+function sick() {
+  if(resetter != 2){background(Color); resetter=2}
+  for(var i = 0; i<laenge; i++){
+    if(healthstatus[i] == 1){
+    noFill();
+    strokeWeight(1);
+    stroke(r[i],g[i],b[i]);
+
+    var x = radius[i];
+    // "S-c* a^x" beschränktes Wachstum, um unendlich große Kreise zu verhindern -- umso höher die Werte für radius, je geringer der unterschied zur nächstgrößeren größe
+    //var rFormel = S-k* a^(x*10); -- habe mich letztendlich dagegen entschieden, da dadurch die weit entfernten distanzen extrem miteinander verschwimmen
+    var rFormel = k * x;
+    ellipse(150,300, rFormel, rFormel);
+  }
+}
+}
+
+
+function healthy() {
+  if(resetter != 3){background(Color); resetter = 3}
+  for(var i = 0; i<laenge; i++){
+    if(healthstatus[i] == 0){
+    noFill();
+    strokeWeight(1);
+    stroke(r[i],g[i],b[i]);
+
+    var x = radius[i];
+    // "S-c* a^x" beschränktes Wachstum, um unendlich große Kreise zu verhindern -- umso höher die Werte für radius, je geringer der unterschied zur nächstgrößeren größe
+    //var rFormel = S-k* a^(x*10); -- habe mich letztendlich dagegen entschieden, da dadurch die weit entfernten distanzen extrem miteinander verschwimmen
+    var rFormel = k * x;
+    ellipse(150,300, rFormel, rFormel);
+  }
+}
+}
+
+function draw(){
+  if(document.getElementById('sall').checked){sall();}
+  if(document.getElementById('healthy').checked){healthy();}
+  if(document.getElementById('sick').checked){sick();}
+}
